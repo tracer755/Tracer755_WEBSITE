39e458d00c2f548f1814257220d62a3a8726222e
Tracer755 <44651512+tracer755@users.noreply.github.com>
2024-06-25 00:13:25 -0600

    Update to allow git start

---
+++ start_web.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const helmet = require('helmet');
+
+const app = express();
+const port = 8080;
+
+app.use(helmet());
+app.disable('x-powered-by');
+app.use(express.static('Website'));
+
+app.use((req, res, next) => {
+  res.status(404).send("404 Sorry can't find that!")
+})
+
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send('500 Something broke! Sorry :(')
+})
+
+app.listen(port)
