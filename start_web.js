5bc933fb0eee74632a1ee79febc1e59ed5eb3fd3
Tracer755 <44651512+tracer755@users.noreply.github.com>
2024-06-25 00:32:57 -0600

    logging

--- start_web.js
+++ start_web.js
@@ -1,13 +1,42 @@
 const express = require("express");
 const helmet = require('helmet');
+const winston = require('winston');
+const { combine, timestamp, json, printf } = winston.format;
+
 
 const app = express();
 const port = 3000;
 
+const logger = winston.createLogger({
+  level: 'info',
+  format: combine(timestamp(), json()),
+  transports: [
+    new winston.transports.Console({
+      format: combine(
+        winston.format.colorize(),
+        winston.format.simple(),
+        winston.format.timestamp(),
+        winston.format.cli()
+      ),
+    }),
+    new winston.transports.File({
+      filename: 'logs/log.txt',
+      datePattern: 'YYYY-MM-DD',
+      format: json(), // Use only JSON format for file logging
+    }),
+  ],
+});
+
 app.use((req, res, next) => {
-  console.log(req);
+  if(!req.url.endsWith(".jpg") && !req.url.endsWith(".png") && !req.url.endsWith(".jpeg") && !req.url.endsWith(".ico")){
+    if(req.get('User-Agent') != "Statping-ng"){
+      var ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress 
+      logger.info(`Received request for: ${req.url} | Client ${req.get('User-Agent')} | IP: ${ip}`);
+    }
+  }
   next();
 });
+
 app.use(helmet());
 app.disable('x-powered-by');
 app.use(express.static('Website'));
