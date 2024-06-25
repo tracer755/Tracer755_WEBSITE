9f2f4bb8277105c3c7e2c5e6fffe535576ef7143
Tracer755 <44651512+tracer755@users.noreply.github.com>
2024-06-25 00:49:45 -0600

    Update start_web.js

--- start_web.js
+++ start_web.js
@@ -40,9 +40,9 @@ app.use((req, res, next) => {
 app.use(helmet());
 app.disable('x-powered-by');
 
-app.use((req, res, next) => {
+/*app.use((req, res, next) => {
   res.status(404).send("404 Sorry can't find that!")
-})
+})*/
 
 app.use((err, req, res, next) => {
   console.error(err.stack)
