f46835b1540637e2a8b0298d5825a643dd75564b
Tracer755 <44651512+tracer755@users.noreply.github.com>
2024-06-25 00:58:18 -0600

    test

--- start_web.js
+++ start_web.js
@@ -30,9 +30,9 @@ const logger = winston.createLogger({
 });
 
 app.use((req, res, next) => {
-  if(!req.url.endsWith(".jpg") && !req.url.endsWith(".png") && !req.url.endsWith(".jpeg") && !req.url.endsWith(".ico")){
-    if(req.get('User-Agent') != "Statping-ng"){
-      var ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress 
+  if (!req.url.endsWith(".jpg") && !req.url.endsWith(".png") && !req.url.endsWith(".jpeg") && !req.url.endsWith(".ico")) {
+    if (req.get('User-Agent') != "Statping-ng") {
+      var ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
       logger.info(`Received request for: ${req.url} | Client ${req.get('User-Agent')} | IP: ${ip}`);
     }
   }
@@ -42,17 +42,23 @@ app.use((req, res, next) => {
 app.use(helmet());
 app.disable('x-powered-by');
 
-app.use((req, res, next) => {
-  res.status(404).send("404 Sorry can't find that!")
-})
-
-app.use((err, req, res, next) => {
-  console.error(err.stack)
-  res.status(500).send('500 Something broke! Sorry :(')
-})
-
+// Serve static files from the 'Website' directory
 app.use(express.static('Website'));
 
-app.listen(port)
+// Handle 404 errors
+app.use((req, res, next) => {
+  res.status(404).send("404 Sorry can't find that!");
+});
 
+// Handle other errors
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('500 Something broke! Sorry :(');
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
+
+// Create an HTTP server to listen on port 80
 http.createServer(app).listen(80);
