2eb17941e5e43369cb7ab74f7de3f92b1686df27
Tracer755 <44651512+tracer755@users.noreply.github.com>
2024-06-25 01:29:56 -0600

    rt

--- start_web.js
+++ start_web.js
@@ -3,10 +3,8 @@ const helmet = require('helmet');
 const winston = require('winston');
 const { combine, timestamp, json, printf } = winston.format;
 const http = require('http');
-var cors = require("cors");
 
 const app = express();
-app.use(cors());
 const port = 8080;
 
 const logger = winston.createLogger({
@@ -30,9 +28,9 @@ const logger = winston.createLogger({
 });
 
 app.use((req, res, next) => {
-  if (!req.url.endsWith(".jpg") && !req.url.endsWith(".png") && !req.url.endsWith(".jpeg") && !req.url.endsWith(".ico")) {
-    if (req.get('User-Agent') != "Statping-ng") {
-      var ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+  if(!req.url.endsWith(".jpg") && !req.url.endsWith(".png") && !req.url.endsWith(".jpeg") && !req.url.endsWith(".ico")){
+    if(req.get('User-Agent') != "Statping-ng"){
+      var ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress 
       logger.info(`Received request for: ${req.url} | Client ${req.get('User-Agent')} | IP: ${ip}`);
     }
   }
@@ -41,28 +39,19 @@ app.use((req, res, next) => {
 
 app.use(helmet());
 app.disable('x-powered-by');
+app.use(express.static('Website'));
 
-// Serve static files from the 'Website' directory
-app.use(express.static('./Website'));
-
-app.get("/test", (req,res) => {
-  res.send("./Website/index.html");
+app.use((req, res, next) => {
+  res.status(404).send("404 Sorry can't find that!")
 })
 
-// Handle 404 errors
-app.use((req, res, next) => {
-  res.status(404).send("404 Sorry can't find that!");
-});
-
-// Handle other errors
 app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('500 Something broke! Sorry :(');
-});
+  console.error(err.stack)
+  res.status(500).send('500 Something broke! Sorry :(')
+})
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
-});
 
-// Create an HTTP server to listen on port 80
+
+app.listen(port)
+
 http.createServer(app).listen(80);
