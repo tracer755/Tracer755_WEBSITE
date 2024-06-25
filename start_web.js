b7974946f844a6be3f2906f693df44ea47c84c11
Tracer755 <44651512+tracer755@users.noreply.github.com>
2024-06-25 00:26:14 -0600

    Update start_web.js

--- start_web.js
+++ start_web.js
@@ -2,7 +2,7 @@ const express = require("express");
 const helmet = require('helmet');
 
 const app = express();
-const port = 80;
+const port = 3000;
 
 app.use(helmet());
 app.disable('x-powered-by');
