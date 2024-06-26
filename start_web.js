8e4522cb16369f351d5accc9bdea1f0964fc59c8
Tracer755 <44651512+tracer755@users.noreply.github.com>
2024-06-26 01:20:34 -0600

    fix

--- start_web.js
+++ start_web.js
@@ -3,9 +3,13 @@ const helmet = require('helmet');
 const winston = require('winston');
 const { combine, timestamp, json, printf } = winston.format;
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
 const port = 8080;
+const logging = false;
+
 
 const logger = winston.createLogger({
   level: 'info',
@@ -28,18 +32,55 @@ const logger = winston.createLogger({
 });
 
 app.use((req, res, next) => {
-  if(!req.url.endsWith(".jpg") && !req.url.endsWith(".png") && !req.url.endsWith(".jpeg") && !req.url.endsWith(".ico")){
-    if(req.get('User-Agent') != "Statping-ng"){
-      var ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress 
-      logger.info(`Received request for: ${req.url} | Client ${req.get('User-Agent')} | IP: ${ip}`);
+  if(logging){
+    if(!req.url.endsWith(".jpg") && !req.url.endsWith(".png") && !req.url.endsWith(".jpeg") && !req.url.endsWith(".ico")){
+      if(req.get('User-Agent') != "Statping-ng"){
+        var ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress 
+        logger.info(`Received request for: ${req.url} | Client ${req.get('User-Agent')} | IP: ${ip}`);
+      }
     }
   }
   next();
 });
 
-app.use(helmet());
+//app.use(helmet());
 app.disable('x-powered-by');
-app.use(express.static('Website'));
+
+async function checkFileInFolder(folder, filename) {
+  const filePath = path.join(folder, filename);
+  try {
+    await fs.access(filePath);
+    console.log('File exists');
+    return true;
+  } catch (error) {
+    console.log('File does not exist' + filename);
+    return false;
+  }
+}
+
+app.use((req, res, next) => {
+  console.log(req.url);
+  //try to find the file, if not found continue if it is found serve it. Thank fuck sherlock
+  let filepath = "";
+  if(fs.existsSync(__dirname + "/Website" + req.url)){
+    filepath = __dirname + "/Website" + req.url;
+  }
+  else{
+    if(fs.existsSync(__dirname + "/Website" + req.url + ".html")){
+      filepath = __dirname + "/Website" + req.url + ".html";
+    }
+  }
+
+  if(filepath != ""){
+    if(checkFileInFolder("./Website", filepath)){
+      res.sendFile(filepath);
+    }
+  }
+  else{
+    //next();
+  }
+});
+
 
 app.use((req, res, next) => {
   res.status(404).send("404 Sorry can't find that!")
